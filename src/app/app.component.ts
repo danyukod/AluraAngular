46050c9e5bed079f7b1f6384f89efc49c3888c54
DaniloKodavara <danilo.kodavara@hotmail.com>
2019-05-29 22:58:49 -0300

    Criando o componente photo-form e utilizando o roteamento para exibicao de paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  photos: any[] = [];
+export class AppComponent{
   
-  constructor(photoSerice: PhotoService){
-
-    photoSerice
-      .listFromUser('flavio')
-      .subscribe(photos => 
-        {
-          console.log(photos[0].userId);
-          this.photos = photos
-        });
-    } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
